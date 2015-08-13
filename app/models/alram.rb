91d9d23a0577cc0b75dfa4ccbffbf81aa7c1c5f1
daram79 <mayin1q84@gmail.com>
2015-08-13 12:43:48 +0900

    푸쉬 쓰레드로 바꿈

--- app/models/alram.rb
+++ app/models/alram.rb
@@ -3,4 +3,16 @@ class Alram < ActiveRecord::Base
   belongs_to :send_user, :foreign_key => "send_user_id", :class_name => "User"
   
   belongs_to :alram, :polymorphic => true
+  
+  after_create :after_create_alram
+  
+  def after_create_alram
+    Thread.new do
+      gcm = GCM.new("AIzaSyCTngq3qlQObAe4-uaWapN3qAfex-Ej75s")
+      registration_id = User.where(id: self.user_id).pluck(:registration_id)
+      response = gcm.send(registration_id)
+      self.update(send_flg: true)
+    end
+  end
+  
 end
