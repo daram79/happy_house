6585e49504fff1854bcc18f87436b12ef1b9c3f1
daram79 <mayin1q84@gmail.com>
2015-08-19 23:55:06 +0900

    setting root index

--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'feeds#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
