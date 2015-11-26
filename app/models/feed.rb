f4ed7db585a4bdde9a1c50b0cf08e64a819bdcda
daram79 <mayin1q84@gmail.com>
2015-11-27 04:09:57 +0900

    이름 없는 회원 버그 수정

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -49,8 +49,12 @@ class Feed < ActiveRecord::Base
   
   def create_alram
     user_ids = User.where(alram_on: true).pluck(:id)
-    user_ids.delete(self.user_id)
-    user_ids.each do |user_id|
+    user_cover_ids = UserCover.where("name is not null").pluck(:user_id)
+    
+    send_user_ids = user_ids & user_cover_ids
+    
+    send_user_ids.delete(self.user_id)
+    send_user_ids.each do |user_id|
       self.alram.create(user_id: user_id, send_user_id: self.user_id, push_msg: self.user.user_cover.name + "님이 사진을 올렸습니다.")
     end
   end
--- app/views/alrams/get_alram_data.json.jbuilder
+++ app/views/alrams/get_alram_data.json.jbuilder
@@ -1,3 +1,7 @@
 json.extract! @alram, :alram_type, :send_user_id
-json.send_user_name @alram.send_user.user_cover.name
+if @alram.alram_type == "User"
+	json.send_user_name ""
+else
+	json.send_user_name @alram.send_user.user_cover.name
+end
 json.push_msg @alram.push_msg
